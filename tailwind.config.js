9aa88da6a12e775f029e89309b48f4ccdf4d8c62
Baraa Damer <baraa01damer@gmail.com>
2025-01-26 23:15:16 -0800

    2255- finished tailwind, font, and globals.css config, began work on header, navs, framer motion transitions.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,16 +17,17 @@ export default {
 			lg: '960px',
 			xl: '1200px',
 		},
+		fontFamily: {
+			primary: "var(--font-jetbrainsMono)",
+		},
 		extend: {
 			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)'
+				primary: "#1c1c22",
+				accent: {
+					DEFAULT: "#00ff99",
+					hover: "#00e187"
+				}
 			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
-			}
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
